9db1b3b8cde37c08850fe9f96625a960b77acb79
Hungumn <hung10m@gmail.com>
2022-06-26 12:11:19 +0700

    Router structure folder

---
+++ app/routers/products.router.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const productRoute = express.Router();
+
+let productList = [
+    {
+        "id":1,
+        "fullName":"MacBook Air with M1 chip",
+        "amount":10,
+        "price":999,
+        "sale":10
+    },
+    {
+        "id":2,
+        "fullName":"MacBook Air with M2 chip",
+        "amount":3,
+        "price":1999,
+        "sale":1
+    },
+    {
+        "id":3,
+        "fullName":"iPhone 13 ProMax",
+        "amount":10,
+        "price":1350,
+        "sale":12
+    },
+    {
+        "id":4,
+        "fullName":"iPhone 12 Pro",
+        "amount":10,
+        "price":799,
+        "sale":20
+    },
+    {
+        "id":5,
+        "fullName":"iPad Pro 11-inch",
+        "amount":66,
+        "price":699,
+        "sale":10
+    },
+];
+
+
+productRoute.get("/", (req, res) => {
+    res.status(200).send(productList);
+});
+
+productRoute.get('/:id', (req, res) => {
+    const {id} = req.params;
+    const index = productList.findIndex((product) => product.id == id)
+    if (index != -1){
+        const product = productList[index]
+        res.status(200).send(product)
+    }else{
+        res.status(404).send("Not Found Product number " +id);
+    }
+});
+
+productRoute.post('/', (req, res) => {
+    let product =req.body;
+    product = {
+        id: Math.floor(Math.random() * 100) + 5,
+        ...product
+    }
+    productList= [...productList,product];
+    res.status(201).send(product);
+});
+
+productRoute.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const {fullName, amount, price,sale} = req.body;
+    const index = productList.findIndex((product) => product.id == id)
+    if (index != -1){
+        const oldProduct = productList[index];
+        const updateProduct = {...oldProduct, fullName, amount, price,sale}
+        productList[index]= updateProduct;
+        res.status(200).send(updateProduct);
+    }else{
+        res.status(404).send("Not Found Product number " +id);
+    }
+    
+})
+
+productRoute.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    const index = productList.findIndex((product) => product.id == id)
+    if (index != -1){
+        product = productList[index];
+        productList.splice(index, 1);
+        res.status(200).send(product)
+    }else{
+        res.status(404).send("Not Found Product number " +id);
+    }
+})
+
+module.exports = productRoute;
